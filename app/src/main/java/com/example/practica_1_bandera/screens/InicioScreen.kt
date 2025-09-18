138ca0e0f42a165b4df5c695a93df595dba23426
PalomaPortillo <24170057@uttcampus.edu.mx>
2025-09-18 14:36:14 -0600

    hice cambios en InicioScreen

--- app/src/main/java/com/example/practica_1_bandera/screens/InicioScreen.kt
+++ app/src/main/java/com/example/practica_1_bandera/screens/InicioScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,13 +14,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.practica_1_bandera.ui.theme.Practica_1_BanderaTheme
-
-@Composable
-fun InicioScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier){
-        Text(text = "Inicio Screen")
-    }
-}
 
 @Composable
 fun JaponScreen(modifier: Modifier = Modifier) {
