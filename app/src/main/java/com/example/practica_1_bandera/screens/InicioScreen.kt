e039eff16c3d58af1000a86c3585d94b8144a245
PalomaPortillo <24170057@uttcampus.edu.mx>
2025-09-21 21:29:44 -0600

    agregue una imagen

--- app/src/main/java/com/example/practica_1_bandera/screens/InicioScreen.kt
+++ app/src/main/java/com/example/practica_1_bandera/screens/InicioScreen.kt
@@ -1,19 +1,21 @@
 package com.example.practica_1_bandera.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.practica_1_bandera.ui.theme.Practica_1_BanderaTheme
+
 
 @Composable
 fun ArgentinaScreen(modifier: Modifier = Modifier) {
@@ -35,11 +37,10 @@ fun ArgentinaScreen(modifier: Modifier = Modifier) {
                 .background(white),
                 contentAlignment = Alignment.Center
         ){
-            Box(
-                modifier
-                    .size(50.dp)
-                    .fillMaxWidth().height(200.dp)
-                    .background(white)
+            Image(
+                painter = painterResource(id = R.drawable.sol_de_mayo),
+                contentDescription = "Sol de Mayo",
+                modifier = Modifier.size(120.dp) // ajusta el tamaño
             )
         }
 
